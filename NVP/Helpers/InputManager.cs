22d91b9b98af09f9bcf6bd155d1b5e728a6b735e
Deecellar <19101das@gmail.com>
2019-02-28 11:04:46 -0500

    Drawing Manager no longer needed (Monogame Extended provides that functionality) InputManager Added, Not finished yet but should be in charge of movement configuration, control, events GameSelectedLevel Have the ground to charge Maps onto the screen, have not been proven yet but will be done after InputManager ScreenManager Some lines changed to represent the abstract class (protected and overriden methods) changes in packages.config and some proyects things

---
+++ NVP/Helpers/InputManager.cs
@@ -0,0 +1,80 @@
+﻿
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoGame.Extended.Input.InputListeners;
+using MonoGame.Extended.Input;
+namespace NVP.Helpers
+{
+    public class InputManager : MonoGame.Extended.SimpleGameComponent
+    {
+        public Action<GamePadEventArgs> GamePadFunc { get; set; }
+        public Action<KeyboardEventArgs> KeyboardFunc { get; set; }
+        public Action<MouseEventArgs> MouseFunc { get; set; }
+        public Action<MouseEventArgs> MouseDragFunc { get; set; }
+
+        GamePadListener GamePad;
+        KeyboardListener KeyboardListener;
+        MouseListener MouseListener;
+        List<InputListener> InputsList;
+        public InputManager(Game game) 
+        {
+            
+            GamePadListenerSettings gamePad = new GamePadListenerSettings();
+            KeyboardListenerSettings keyboard = new KeyboardListenerSettings();
+            MouseListenerSettings mouseListener = new MouseListenerSettings();
+
+
+            #region Configurando Controllers
+            keyboard.RepeatPress = false;
+            gamePad.PlayerIndex = PlayerIndex.One;
+            gamePad.VibrationEnabled = false;
+            mouseListener.ViewportAdapter = new MonoGame.Extended.ViewportAdapters.BoxingViewportAdapter(game.Window, game.GraphicsDevice, game.Window.ClientBounds.X, game.Window.ClientBounds.Y);
+            #endregion
+            #region Poniendolos en lista
+            InputsList = new List<InputListener>();
+            InputsList.Add( GamePad = gamePad.CreateListener());
+            InputsList.Add(KeyboardListener = keyboard.CreateListener());
+            InputsList.Add(MouseListener = mouseListener.CreateListener());
+            #endregion
+
+            #region Configurando eventos
+
+            GamePad.ButtonDown += GamePad_ButtonDown;
+            KeyboardListener.KeyPressed += KeyboardListener_KeyPressed;
+            MouseListener.MouseClicked += MouseListener_MouseClicked;
+            
+            #endregion
+
+            game.Components.Add(new InputListenerComponent(game, InputsList.ToArray()));
+        }
+
+        private void MouseListener_MouseClicked(object sender, MouseEventArgs e)
+        {
+            MouseFunc.Invoke(e);
+        }
+
+        private void KeyboardListener_KeyPressed(object sender, KeyboardEventArgs e)
+        {
+            KeyboardFunc.Invoke(e);
+        }
+
+        private void GamePad_ButtonDown(object sender, GamePadEventArgs e)
+        {
+            GamePadFunc.Invoke(e)
+        }
+
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach(var input in InputsList)
+            {
+                input.Update(gameTime);
+            }
+
+        }
+    }
+}
---
+++ NVP/Screen/GameSelectedLevel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using MonoGame.Extended.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Graphics;
+
+namespace NVP.Screen
+{
+    class GameSelectedLevel : GameScreen
+    {
+        // The tile map
+        private TiledMap map;
+        // The renderer for the map
+        private TiledMapRenderer mapRenderer;
+
+        private Camera2D camera;
+
+        private SpriteBatch spriteBatch;
+
+        public GameSelectedLevel(Game game, int level) : base(game)
+        {
+            var viewport = new MonoGame.Extended.ViewportAdapters.BoxingViewportAdapter(Game.Window, GraphicsDevice, Game.Window.ClientBounds.X, Game.Window.ClientBounds.Y);
+            camera = new Camera2D(viewport);
+            spriteBatch = new SpriteBatch(GraphicsDevice); 
+            
+            ChargeLevel(level);
+        }
+
+        private void ChargeLevel(int level)
+        {
+            map = Content.Load<TiledMap>("Maps/level"+level);
+            mapRenderer = new TiledMapRenderer(GraphicsDevice);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin(transformMatrix: camera.GetViewMatrix(), samplerState: SamplerState.PointClamp);
+            mapRenderer.Draw(map, camera.GetViewMatrix());
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            mapRenderer.Update(map, gameTime);
+        }
+    }
+}
